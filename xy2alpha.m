a793e4a901baaf41f8386c1184a7f7190c77f9b7
Raphael <48169917+rapsar@users.noreply.github.com>
2020-08-25 11:35:08 -0600

    Update xy2alpha.m

--- xy2alpha.m
+++ xy2alpha.m
@@ -1,13 +1,13 @@
 function [alpha] = xy2alpha(xy,v)
-%XY2ALPHA Converts the xy coordinates in the equirectangular frame to
-%   spherical coordinates alpha
+%XY2ALPHA Converts the xy (Nx2) coordinates in the equirectangular frame to
+%   spherical projections alpha (Nx3)
 %
-%   xy is a list of positions (Nx2, subsequent colums are not considered)
+%   xy is list of positions (Nx(2+p), subsequent p columns added at the end)
 %   v informs about size of equirectangular frames; it is either: 
 %       - a movie structure 
 %       - an array containing [width height]
 %
-%   alpha is Nx3 array
+%   alpha is Nx(3+p) array (alpha projections are first 3 columns)
 %   
 %
 % Raphael Sarfati, 03/2020
@@ -15,7 +15,6 @@ function [alpha] = xy2alpha(xy,v)
 
 [theta,phi] = xy2thetaphi(xy,v);
 
-alpha = horzcat(cos(theta).*sin(phi),sin(theta).*sin(phi),cos(phi));
+alpha = horzcat(cos(theta).*sin(phi),sin(theta).*sin(phi),cos(phi),xy(:,3:end));
 
 end
-
